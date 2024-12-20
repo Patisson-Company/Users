eaef7bf59d59868789bb68e1f3db5bbbe2aa1b32
EliseyGodX <krotov.ruslan.31@gmail.com>
2024-12-20 23:03:57 +0300

    v0.1.1

--- app/main.py
+++ app/main.py
@@ -21,15 +21,17 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title=config.SERVICE_NAME, lifespan=lifespan)
 
 if __name__ == "__main__":
+    health_path = f'/{config.SERVICE_NAME}/{UsersRoute.health().path}'
+    
     app_launcher = UvicornFastapiAppLauncher(app, router,
                         service_name=config.SERVICE_NAME,
                         host=config.SERVICE_HOST)
     app_launcher.add_token_middleware(
         config.SelfService.get_access_token,
-        excluded_paths=[f'/{UsersRoute.health().path}']
+        excluded_paths=[health_path]
         )
     app_launcher.add_sync_consul_health_path()
-    app_launcher.consul_register()
+    app_launcher.consul_register(health_path)
     app_launcher.add_jaeger()
     app_launcher.add_route(
         path='/graphql', 
